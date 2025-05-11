984a3c649200a9f586c104c503616b6ff0143fd6
mdnaeemmiah <153842332+mdnaeemmiah@users.noreply.github.com>
2025-05-12 01:46:12 +0600

    added

--- src/app/(mainLayout)/page.tsx
+++ src/app/(mainLayout)/page.tsx
@@ -1,7 +1,7 @@
 
 import Banner from '@/components/home/Banner';
 import Blog from '@/components/home/Blog';
-import Cart from '@/components/home/Cart';
+// import Cart from '@/components/home/Cart';
 import Skils from '@/components/home/Skils';
 import Carousal from '@/components/home/carousal';
 import React from 'react';
@@ -13,7 +13,7 @@ const HomePage = () => {
     <div className='text-center  mt-28 '>
       <Banner></Banner>
       <Skils></Skils>
-      <Cart></Cart>
+      {/* <Cart></Cart> */}
       <Carousal></Carousal>
       <Blog></Blog>
     </div>
