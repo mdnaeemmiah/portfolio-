83767991a6bcd2a9a5b4cdb15d0e60e0423f0a46
mdnaeemmiah <153842332+mdnaeemmiah@users.noreply.github.com>
2025-05-14 13:00:31 +0600

    added

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -1,12 +1,29 @@
 import Link from "next/link";
 import { AiOutlineDashboard } from "react-icons/ai";
-import { BiBook, BiEdit, BiListCheck, BiFolderOpen, BiPlusCircle, BiTask, BiHome } from "react-icons/bi";
+import {
+  BiBook,
+  BiEdit,
+  BiListCheck,
+  BiFolderOpen,
+  BiPlusCircle,
+  BiTask,
+  BiHome,
+} from "react-icons/bi";
 import { FaTasks } from "react-icons/fa";
 
 const Sidebar = () => {
   return (
     <div className="bg-slate-100 min-h-screen p-4 pt-12 rounded-xl">
       <ul className="space-y-4">
+        <li>
+          <Link
+            href="/"
+            className="flex items-center space-x-2 p-3 rounded-md hover:bg-gray-200 text-gray-700"
+          >
+            <BiHome className="h-5 w-5" />
+            <span>Home</span>
+          </Link>
+        </li>
         <li>
           <Link
             href="/hello"
@@ -77,15 +94,6 @@ const Sidebar = () => {
           >
             <BiTask className="h-5 w-5" />
             <span>All Message</span>
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/"
-            className="flex items-center space-x-2 p-3 rounded-md hover:bg-gray-200 text-gray-700"
-          >
-            <BiHome className="h-5 w-5" />
-            <span>Home</span>
           </Link>
         </li>
       </ul>
