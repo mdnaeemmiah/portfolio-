0e52ba5f5c5db548efb9ffe37f23c0dbc80fde8c
mdnaeemmiah <153842332+mdnaeemmiah@users.noreply.github.com>
2025-05-15 15:00:00 +0600

    added

--- src/app/(mainLayout)/blog/[blogId]/page.tsx
+++ src/app/(mainLayout)/blog/[blogId]/page.tsx
@@ -2,7 +2,7 @@ import BlogDetails from "@/components/home/BlogDetails";
 
 const BlogDetailsPage = async({params}: {params:Promise<{blogId :string}>}) => {
     const {blogId} = await params;
-    const res =await fetch(`http://localhost:5000/api/blog/${blogId}`);
+    const res =await fetch(`https://l2b4-a5-server.vercel.app/api/blog/${blogId}`);
     const blog =await res.json();
     console.log(blog.data)
     
